a705a465cb3109f6a3f21fe0fd233c734512db2e
Christopher Ewest <christookar@web.de>
2013-08-10 11:16:32 +0200

    JUnit
    Signed-off-by: Christopher Ewest <christookar@web.de>

---
+++ src/test/java/de/tuberlin/uebb/jbop/optimizer/arithmetic/ArithmeticExpressionInterpreterTest.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2013 uebb.tu-berlin.de.
+ * 
+ * This file is part of JBOP (Java Bytecode OPtimizer).
+ * 
+ * JBOP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JBOP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JBOP. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.tuberlin.uebb.jbop.optimizer.arithmetic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
+
+/**
+ * Tests for {@link ArithmeticExpressionInterpreter}.
+ * 
+ * @author Christopher Ewest
+ */
+public class ArithmeticExpressionInterpreterTest {
+  
+  private final ArithmeticExpressionInterpreter interpreter = new ArithmeticExpressionInterpreter();
+  private final MethodNode methodNode = new MethodNode(Opcodes.ACC_PUBLIC, "testMethod", "()V", null, new String[] {});
+  private final InsnList methodInstructions = new InsnList();
+  
+  @Before
+  public void before() {
+    methodNode.instructions = methodInstructions;
+  }
+  
+  /**
+   * Tests that arithmeticExpressionInterpreter() of the Testobject is working correctly
+   * if nothing is todo (empty amethod).
+   */
+  @Test
+  public void testArithmeticExpressionInterpreterEmptyMethod() {
+    // INIT
+    
+    // RUN
+    final InsnList optimized = interpreter.optimize(methodInstructions, methodNode);
+    
+    // ASSERT
+    assertFalse(interpreter.isOptimized());
+    assertEquals(0, optimized.size());
+  }
+  
+  /**
+   * Tests that arithmeticExpressionInterpreter() of the Testobject is working correctly
+   * if nothing is todo (no arithmetic Expression occur in instructions).
+   */
+  @Test
+  public void testArithmeticExpressionInterpreterNoArithmeticInstructions() {
+    // INIT
+    methodInstructions.add(new InsnNode(Opcodes.ICONST_1));
+    methodInstructions.add(new VarInsnNode(Opcodes.ISTORE, 1));
+    methodInstructions.add(new InsnNode(Opcodes.ICONST_2));
+    methodInstructions.add(new VarInsnNode(Opcodes.ISTORE, 2));
+    methodInstructions.add(new InsnNode(Opcodes.ICONST_3));
+    methodInstructions.add(new VarInsnNode(Opcodes.ISTORE, 3));
+    methodInstructions.add(new InsnNode(Opcodes.RETURN));
+    // RUN
+    final InsnList optimized = interpreter.optimize(methodInstructions, methodNode);
+    
+    // ASSERT
+    assertFalse(interpreter.isOptimized());
+    assertEquals(7, optimized.size());
+  }
+  
+  /**
+   * Tests that arithmeticExpressionInterpreter() of the Testobject is working correctly
+   * if arithmetic Expression occur in instructions.
+   */
+  @Test
+  public void testArithmeticExpressionInterpreter() {
+    // INIT
+    methodInstructions.add(new InsnNode(Opcodes.ICONST_1));
+    methodInstructions.add(new InsnNode(Opcodes.ICONST_2));
+    methodInstructions.add(new InsnNode(Opcodes.IADD));
+    methodInstructions.add(new InsnNode(Opcodes.ICONST_1));
+    methodInstructions.add(new InsnNode(Opcodes.ICONST_1));
+    methodInstructions.add(new InsnNode(Opcodes.IADD));
+    methodInstructions.add(new InsnNode(Opcodes.IADD));
+    methodInstructions.add(new InsnNode(Opcodes.RETURN));
+    // RUN STEP1
+    final InsnList optimized = interpreter.optimize(methodInstructions, methodNode);
+    
+    // ASSERT STEP 1
+    assertTrue(interpreter.isOptimized());
+    assertEquals(4, optimized.size());
+    assertEquals(Opcodes.ICONST_3, optimized.getFirst().getOpcode());
+    assertEquals(Opcodes.ICONST_2, optimized.getFirst().getNext().getOpcode());
+    
+    // RUN STEP 2
+    final InsnList optimized2 = interpreter.optimize(methodInstructions, methodNode);
+    
+    // ASSERT STEP 2
+    assertTrue(interpreter.isOptimized());
+    assertEquals(2, optimized2.size());
+    assertEquals(Opcodes.ICONST_5, optimized2.getFirst().getOpcode());
+    
+    // RUN STEP 3
+    final InsnList optimized3 = interpreter.optimize(methodInstructions, methodNode);
+    
+    // ASSERT STEP 3
+    assertFalse(interpreter.isOptimized());
+    assertEquals(2, optimized3.size());
+    assertEquals(Opcodes.ICONST_5, optimized2.getFirst().getOpcode());
+  }
+}
