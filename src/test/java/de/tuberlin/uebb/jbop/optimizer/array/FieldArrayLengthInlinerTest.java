ace9a86aeb046dccafaa904fc09dc89ac1e86ac1
Christopher Ewest <christookar@web.de>
2013-08-11 21:03:37 +0200

    Javadoc
    Signed-off-by: Christopher Ewest <christookar@web.de>

--- src/test/java/de/tuberlin/uebb/jbop/optimizer/array/FieldArrayLengthInlinerTest.java
+++ src/test/java/de/tuberlin/uebb/jbop/optimizer/array/FieldArrayLengthInlinerTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2013 uebb.tu-berlin.de.
+ * 
+ * This file is part of JBOP (Java Bytecode OPtimizer).
+ * 
+ * JBOP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JBOP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JBOP. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.tuberlin.uebb.jbop.optimizer.array;
 
 import static org.junit.Assert.assertEquals;
@@ -18,8 +36,14 @@ import de.tuberlin.uebb.jbop.optimizer.utils.NodeHelper;
 
 public class FieldArrayLengthInlinerTest {
   
+  /**
+   * Tests that FieldArrayLengthInbliner is working correctly.
+   * 
+   * @throws Exception
+   *           the exception
+   */
   @Test
-  public void testFieldArrayLengthInbliner() throws Exception {
+  public void testFieldArrayLengthInliner() throws Exception {
     // INIT
     final String owner = "de.tuberlin.uebb.jbop.optimizer.array.FieldArrayLengthTestClass";
     final ClassNodeBuilder builder = ClassNodeBuilder.createClass(owner).//
