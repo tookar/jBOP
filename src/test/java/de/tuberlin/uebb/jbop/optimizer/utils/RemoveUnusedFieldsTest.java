971ce88b9f3e5ddc8d6a6d52d265544f979d549d
Christoph Höger <christoph.hoeger@tu-berlin.de>
2013-10-07 15:23:10 +0200

    Add a (failing) test for slightly complex constructors
    The problem seems to be the "cleaning" of the stack before a
    putfield instruction. Obviously the interpreter is either ignoring
    branches or gets the stack layout wrong.
    
    Signed-off-by: Christoph Höger <christoph.hoeger@tu-berlin.de>

--- src/test/java/de/tuberlin/uebb/jbop/optimizer/utils/RemoveUnusedFieldsTest.java
+++ src/test/java/de/tuberlin/uebb/jbop/optimizer/utils/RemoveUnusedFieldsTest.java
@@ -1,26 +1,34 @@
 package de.tuberlin.uebb.jbop.optimizer.utils;
 
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.JumpInsnNode;
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.ParameterNode;
+import org.objectweb.asm.tree.VarInsnNode;
+
+import de.tuberlin.uebb.jbop.optimizer.ClassNodeBuilder;
 import static org.junit.Assert.assertEquals;
 import static org.objectweb.asm.Opcodes.ACC_PRIVATE;
 import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
 import static org.objectweb.asm.Opcodes.ACC_SYNTHETIC;
 import static org.objectweb.asm.Opcodes.ALOAD;
 import static org.objectweb.asm.Opcodes.GETFIELD;
+import static org.objectweb.asm.Opcodes.GOTO;
 import static org.objectweb.asm.Opcodes.IADD;
+import static org.objectweb.asm.Opcodes.IFEQ;
+import static org.objectweb.asm.Opcodes.ILOAD;
 import static org.objectweb.asm.Opcodes.INVOKESPECIAL;
 import static org.objectweb.asm.Opcodes.INVOKESTATIC;
 import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
 import static org.objectweb.asm.Opcodes.PUTFIELD;
-
-import org.junit.Test;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldInsnNode;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
-import org.objectweb.asm.tree.VarInsnNode;
-
-import de.tuberlin.uebb.jbop.optimizer.ClassNodeBuilder;
 
 public class RemoveUnusedFieldsTest {
   
@@ -79,7 +87,7 @@ public class RemoveUnusedFieldsTest {
         addToConstructor(new FieldInsnNode(GETFIELD, owner, "field1", "I")).//
         addToConstructor(new InsnNode(IADD)).//
         addToConstructor(new VarInsnNode(ALOAD, 1)).//
-        addToConstructor(new MethodInsnNode(INVOKEVIRTUAL, "java/lang/Object", "hasCode", "()I")).//
+        addToConstructor(new MethodInsnNode(INVOKEVIRTUAL, "java/lang/Object", "hashCode", "()I")).//
         addToConstructor(new InsnNode(IADD)).//
         addToConstructor(new FieldInsnNode(PUTFIELD, owner, "field3", "I"));//
     classNode = classBuilder.getClassNode();
@@ -92,5 +100,61 @@ public class RemoveUnusedFieldsTest {
     assertEquals(3, constructor.instructions.size());
     
     classBuilder.instance();
+  }
+
+  @Test
+  /**
+   * Test the removal code against if-branches.
+   * @throws Exception
+   */
+  public void test3() throws Exception {    
+    // INIT
+    final String owner = "de/tuberlin/uebb/jbop/optimizer/utils/TestClass3";
+    final LabelNode label_then = new LabelNode();
+    final LabelNode label_endif = new LabelNode();
+    
+    classBuilder = ClassNodeBuilder.createClass("de.tuberlin.uebb.jbop.optimizer.utils.TestClass3").//
+        addField("field1", "I");
+    
+    final MethodNode constructor = classBuilder.addMethod("<init>", "(II)V")
+    .addInsn(new VarInsnNode(Opcodes.ALOAD, 0))
+    .addInsn(
+            new MethodInsnNode(Opcodes.INVOKESPECIAL,
+                    "java/lang/Object" , "<init>", "()V"))
+    .addInsn(new InsnNode(Opcodes.RETURN)).//
+            /* having an ALOAD_0 before the if-branch makes the optimizer run amok */
+        addInsn(new VarInsnNode(ALOAD, 0)).//        
+
+        addInsn(new VarInsnNode(ILOAD, 1)).//        
+        addInsn(new JumpInsnNode(IFEQ, label_then)).
+        addInsn(new VarInsnNode(ILOAD, 2)).//
+        addInsn(new FieldInsnNode(PUTFIELD, owner, "field1", "I")).
+        addInsn(new JumpInsnNode(GOTO, label_endif)).//        
+        addInsn(label_then).//
+        addInsn(new VarInsnNode(ILOAD, 1)).//
+        addInsn(new FieldInsnNode(PUTFIELD, owner, "field1", "I")).
+        addInsn(label_endif).//        
+    returnMethod();
+    
+    classNode = classBuilder.getClassNode();
+    
+    // RUN
+    RemoveUnusedFields.removeUnusedFields(classNode);
+    
+    // ASSERT
+    /* Expected:
+       0: aload_0       
+       1: invokespecial #11                 // Method java/lang/Object."<init>":()V
+       4: aload_0
+       5: iload_1       
+       6: ifeq          12
+       9: goto          12
+      12: return  
+    */
+    assertEquals(9, constructor.instructions.size());
+
+    assertEquals(0, classNode.fields.size());
+
+    classBuilder.instance();    
   }
 }
