1f4d68f26f68c8d69214fbdc87adc37c289232ee
Christopher Ewest <christookar@web.de>
2013-11-26 22:00:32 +0100

    Example
    Signed-off-by: Christopher Ewest <christookar@web.de>

--- src/test/java/de/tuberlin/uebb/jbop/example/Example.java
+++ src/test/java/de/tuberlin/uebb/jbop/example/Example.java
@@ -1,10 +1,16 @@
 package de.tuberlin.uebb.jbop.example;
 
+import java.util.logging.Logger;
+
+import org.apache.commons.lang3.builder.CompareToBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import de.tuberlin.uebb.jbop.optimizer.annotations.ImmutableArray;
 import de.tuberlin.uebb.jbop.optimizer.annotations.Optimizable;
 import de.tuberlin.uebb.jbop.optimizer.annotations.StrictLoops;
 
-public class Example implements IExample {
+public class Example implements IExample, Comparable<IExample> {
   
   private final ExampleChain chain;
   @ImmutableArray
@@ -24,20 +30,33 @@ public class Example implements IExample {
     }
     for (int i = 0; i < doubleArray.length; ++i) {
       if (chain.chainArray.length != doubleArray[i].length) {
-        debug("This will crash...");
+        Logger.getLogger("").fine("This will crash...");
       }
     }
     double res = 0;
     for (int i = 0; i < doubleArray.length; ++i) {
       final double[] localDoubleArray = doubleArray[0];
       for (int j = 0; j < localDoubleArray.length; ++j) {
-        res += chain.chainArray[j] + localDoubleArray[j];
+        for (int k = 0; k < 8; ++k) {
+          res += chain.chainArray[j] + localDoubleArray[j];
+        }
       }
     }
     return res;
   }
   
-  private void debug(final String text) {
-    System.out.println(text);
+  @Override
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this, false);
+  }
+  
+  @Override
+  public boolean equals(final Object obj) {
+    return EqualsBuilder.reflectionEquals(this, obj, false);
+  }
+  
+  @Override
+  public int compareTo(final IExample o) {
+    return CompareToBuilder.reflectionCompare(this, o);
   }
 }
