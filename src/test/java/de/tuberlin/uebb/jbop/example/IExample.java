f8d93f28f3c3dc681142326b6a8a87c9a847e92f
Christopher Ewest <christookar@web.de>
2013-12-02 10:56:36 +0100

    removed Benchmarks and examples -> new project jBOP_benchmark
    Signed-off-by: Christopher Ewest <christookar@web.de>

--- src/test/java/de/tuberlin/uebb/jbop/example/IExample.java
+++
@@ -1,6 +0,0 @@
-package de.tuberlin.uebb.jbop.example;
-
-public interface IExample {
-  
-  public double run();
-}
--- src/test/java/de/tuberlin/uebb/jbop/example/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains a modified version of Apaches
- * {@link org.apache.commons.math3.analysis.differentiation.DerivativeStructure} and
- * {@link org.apache.commons.math3.analysis.differentiation.DSCompiler} (commons-math).
- * 
- * This is meant to be an example for the usage of this library.
- */
-package de.tuberlin.uebb.jbop.example;
